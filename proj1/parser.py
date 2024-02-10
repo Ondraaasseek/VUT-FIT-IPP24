28aba079b2cd21a0b37a6a10bd859e1546378a78
Ondřej Novotný <ondronicek@gmail.com>
2024-02-10 21:37:18 +0100

    feat: Not working begin to Objectify and testing file

--- proj1/parser.py
+++ proj1/parser.py
@@ -1,28 +1,32 @@
 import sys
 
+class Argument:
+    def __init__(self, args, type):
+        self.args = args
+        self.type = type
+
+class Instruction:
+    def __init__(self, opcode, *args):
+        self.opcode = opcode
+        self.args = [Argument(arg, type) for arg in args]
+
+
 args = sys.argv
 if len(args) != 1:
     if args[1] == "--help":
-        if len(args) > 2:
+        if len(args) > 2 or sys.stdin == 0:
             sys.exit(10)
-        print("help:")
+        print("--help | Vypíše tuto zprávu")
+        sys.exit(0)
 
-hasHeader = False
 for line in sys.stdin:
     tokens = line.split()
     for i in range(0, len(tokens)):
         if tokens[i].__contains__("#"):
             tokens = tokens[:i]
             break
+    # Skiping empty lines or just comment lines
 
-    # First we need to check for .IPPcode24 Header
-    print(len(tokens), hasHeader, tokens)
-    if hasHeader == False and len(tokens) == 1 and tokens[0] == ".IPPcode24":
-        hasHeader = True
-        break
-    elif hasHeader == False:
-        sys.exit(21)
-    else:
-        # We found header or returned err 21, so now it's time to look for other funny instruction and validate them
-        print("Are we happy?")
+
+
 
